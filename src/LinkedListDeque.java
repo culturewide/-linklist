d93488feb8c66f8ce5ba859fada93ec88f7bb641
culturewide <eatfanbrother@gmail.com>
2025-03-30 17:25:07 +0800

    second commit

--- src/LinkedListDeque.java
+++ src/LinkedListDeque.java
@@ -1,5 +1,25 @@
 public class LinkedListDeque<T> {
-    public void addFirst(T item){
-      int x=0;
+    public int size=0;
+    public class IntNode {
+        public T first;
+        public IntNode next;
+        public IntNode(){
+            first=null;
+            next=null;
+        }
+    public void LinkedListDeque(){
+            sentinel.next=null;
+            
+
+    }
+    public IntNode sentinel=null;
+        public void addFirst(T t) {
+
+        }
+    }
+
+    public static void main (String[] args) {
+        LinkedListDeque list = new LinkedListDeque();
+
     }
 }
